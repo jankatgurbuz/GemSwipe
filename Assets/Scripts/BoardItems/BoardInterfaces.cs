902aab9c9a36654dc8a7f2e37cde0d9e55f1e750
jankatgurbuz <jankatgurbuz@gmail.com>
2024-05-08 10:17:49 +0300

    Implement initial swipe detection

--- Assets/Scripts/BoardItems/BoardInterfaces.cs
+++ Assets/Scripts/BoardItems/BoardInterfaces.cs
@@ -8,21 +8,22 @@ namespace BoardItems
     {
         public TPoolItem Item { get; set; }
     }
-    public interface IBoardItem :IItemBehavior
+
+    public interface IBoardItem : IItemBehavior
     {
         int Row { get; }
         int Column { get; }
+        bool IsGem { get; set; }
         IBoardItemVisitor BoardVisitor { get; set; }
         IBoardItem Copy();
         void RetrieveFromPool();
         void ReturnToPool();
-        
     }
+
     public interface IItemBehavior
     {
         void SetPosition(Vector3 position);
         Vector3 GetPosition();
-        void SetActive(bool active); 
+        void SetActive(bool active);
     }
-    
 }
